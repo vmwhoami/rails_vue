124a3c9412d3de79c6fe84b30f03ecfb8ce6e81a
vmwhoami <vitalie.m@travod.com>
2022-03-08 16:10:33 +0200

    Add vuejs to dockerized rails app

---
+++ app/javascript/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p v-text="message"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Da lucreaza sau nu?"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
